66d213d27c8579bf232c115601dc4587e568ab7e
Dmitriy Poltavchenko <dmitriy@polarbear.partners>
2016-03-28 16:50:05 +0300

    Add blank lines (PEP8)

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 
+
 def before_feature(context, feature):
     context.browser = webdriver.Firefox()
     context.browser.maximize_window()
+
 
 def after_feature(context, feature):
     context.browser.quit()
