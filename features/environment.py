9394423b07476fb79389590d6c5f410c86aa0c71
Dmitriy Poltavchenko <dmitriy@polarbear.partners>
2016-04-07 15:55:10 +0300

    Make screenshot if step is fail

--- features/environment.py
+++ features/environment.py
@@ -6,5 +6,12 @@ def before_feature(context, feature):
     context.browser.maximize_window()
 
 
+def after_step(context, step):
+    if step.status == "failed":
+        screenshot = context.scenario.name + " - " + step.name + ".png"
+        context.browser.save_screenshot(screenshot)
+        print("Screenshot saved to file: %s" % screenshot)
+
+
 def after_feature(context, feature):
     context.browser.quit()
