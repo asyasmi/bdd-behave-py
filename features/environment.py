edd744cb54bb6400825f3898a5bd5a35ef921f35
Dmitriy Poltavchenko <dmitriy@polarbear.partners>
2016-04-12 13:13:53 +0300

    concatenate path using os.path.join

--- features/environment.py
+++ features/environment.py
@@ -3,9 +3,8 @@ import logging
 from selenium import webdriver
 from behave.log_capture import capture
 
-RUNTIME_DIR = os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))
-) + '/runtime/'
+RUNTIME_DIR = os.path.join(
+    os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, 'runtime')))
 SCR_EXT = '.png'
 
 
@@ -19,8 +18,8 @@ def after_step(context, step):
     if step.status == "failed":
         if not os.path.exists(RUNTIME_DIR):
             os.mkdir(RUNTIME_DIR)
-        screenshot = RUNTIME_DIR + \
-            context.scenario.name + " - " + step.name + SCR_EXT
+        screenshot = os.path.join(
+            RUNTIME_DIR, context.scenario.name + " - " + step.name + SCR_EXT)
         context.browser.save_screenshot(screenshot)
         logging.error("Screenshot saved to file: %s" % screenshot)
 
