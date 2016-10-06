21887f6f53b8b440e5846159b2472fd65edc20d7
Dmitriy Poltavchenko <dmitriy@polarbear.partners>
2016-10-06 15:09:38 +0300

    Add GRID_HUB_URL variable

--- features/environment.py
+++ features/environment.py
@@ -3,22 +3,21 @@ import logging
 from selenium import webdriver
 from behave.log_capture import capture
 
+GRID_HUB_URL = os.environ.get('GRID_HUB_URL')
 RUNTIME_DIR = os.path.join(
     os.path.abspath(os.path.join(__file__, os.pardir, os.pardir, 'runtime')))
 SCR_EXT = '.png'
 
 
 def before_all(context):
-    # context.browser = webdriver.Firefox()
-    desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
-    desired_capabilities['version'] = 'latest'
-    desired_capabilities['platform'] = 'LINUX'
-    desired_capabilities['name'] = 'Testing Selenium with Behave'
-
-    context.browser = webdriver.Remote(
-        desired_capabilities=desired_capabilities,
-        command_executor="http://localhost:4444/wd/hub"
-    )
+    if GRID_HUB_URL is None:
+        context.browser = webdriver.Firefox()
+    else:
+        desired_capabilities = webdriver.DesiredCapabilities.FIREFOX
+        context.browser = webdriver.Remote(
+            desired_capabilities=desired_capabilities,
+            command_executor=GRID_HUB_URL
+        )
     context.browser.maximize_window()
 
 
